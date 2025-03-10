506a3465554cfb5d4aa834c2f252d4b59aa98c4a
Dusan Tomic <duletomic14901@gmail.com>
2025-03-10 23:41:48 +0100

    zad6

--- Zadatak6/src/app/sidebaar/sidebaar.component.ts
+++ Zadatak6/src/app/sidebaar/sidebaar.component.ts
@@ -1,18 +1,35 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { NgIf, NgClass } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NgIf],
+  imports: [NgIf, NgClass],
   templateUrl: './sidebaar.component.html',
-  styleUrls: ['./sidebaar.component.css']
+  styleUrls: ['./sidebaar.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({ width: '50px', opacity: 0.5 })),
+      state('expanded', style({ width: '200px', opacity: 1 })),
+      transition('collapsed <=> expanded', animate('0.3s ease-in-out'))
+    ])
+  ],
 })
 export class SidebaarComponent {
+  activeRoute: string = '/home';
   isExpanded: boolean = false;
   @ViewChild('shrink') shrinkIcon!: ElementRef;
 
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      // Kad se ruta promeni, ažuriraj aktivnu rutu
+      this.activeRoute = this.router.url;
+    });
+  }
 
   expand(event: any) {
     this.isExpanded = true;
@@ -24,5 +41,7 @@ export class SidebaarComponent {
 
   navigateTo(route: string) {
     this.router.navigate([route]);
+    this.activeRoute = route;
+    console.log(this.activeRoute)
   }
 }
